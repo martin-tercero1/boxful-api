0cb8946a95149225874fc80647247879c362cf6d
Martin Tercero <martintercero20@hotmail.com>
2026-05-12 15:41:08 -0600

    Update main.ts

--- src/main.ts
+++ src/main.ts
@@ -9,7 +9,7 @@ dotenv.config();
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
-  app.enableCors({ origin: 'http://localhost:3000' }); // Boxful NextJS frontend
+  app.enableCors({ origin: process.env.FRONTEND_URL || 'http://localhost:3000' }); // Boxful NextJS frontend
   app.useGlobalPipes(new ValidationPipe({ whitelist: true, transform: true }));
   app.setGlobalPrefix('api/v1');
 
